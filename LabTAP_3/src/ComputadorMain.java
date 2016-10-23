97e5cd6ff73f81e3a11a98923f255759d2651d62
renatolousan <renatolousan@hotmail.com>
2016-10-23 00:51:31 -0400

    minor change

--- LabTAP_3/src/ComputadorMain.java
+++ LabTAP_3/src/ComputadorMain.java
@@ -3,7 +3,7 @@ public class ComputadorMain {
 	public static void main(String[] args) {
 		Computador comp = new Computador();
 		
-		comp.processador.marca = "Intelli";
+		comp.processador.marca = "Intelligent";
 		
 		
 	}
